66c97341b5cf14aaa1bdfaa62a728cfbe530d34b
DauTT <dautruongtan@gmail.com>
2022-09-01 16:29:12 +0530

    Group all the test setup methods in a class

--- tests/setup.py
+++ tests/setup.py
@@ -1,109 +1,99 @@
 
-from operator import ne
-from typing import Any, Optional
 from terra_sdk.client.localterra import LocalTerra
-from terra_sdk.core import Coins
-from terra_sdk.core.wasm import MsgStoreCode, MsgInstantiateContract, MsgExecuteContract
-from terra_sdk.core.bech32 import AccAddress
-from terra_sdk.client.lcd import LCDClient, Wallet
-from bot.util import TokenAsset, NativeAsset,\
-    Asset, read_artifact, write_artifact
+from bot.type import TokenAsset, NativeAsset
+from bot.util import read_artifact, write_artifact
 from bot.dca import DCA
-from typing import List
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def upload_dca_contract(terra: LCDClient,
-                        wallet: Wallet) -> str:
-    network = read_artifact("localterra")
+class Setup:
 
-    code_id = DCA(terra, wallet).upload_contract(
-        "tests/artifacts/astroport_dca_module.wasm")
-    network["dcaCodeId"] = code_id
-    write_artifact(network, "localterra")
-    return code_id
+    def __init__(self):
+        self.terra = LocalTerra()
+        self.wallet = self.terra.wallets["test1"]
+        self.network = read_artifact("localterra")
 
+    def upload_dca_contract(self) -> str:
+        code_id = DCA(self.terra, self.wallet).upload_contract(
+            "tests/artifacts/astroport_dca_module.wasm")
+        self.network["dcaCodeId"] = code_id
+        write_artifact(self.network, "localterra")
+        return code_id
 
-def instantiate_dca_contract(terra: LCDClient,
-                             wallet: Wallet, dca_code_id: int):
-    network = read_artifact("localterra")
-    instantiate_msg = {
-        "factory_addr":
-        "terra1qnmggqv3u03axw69cn578hukqhl4f2ze2k403ykcdlhj98978u7stv7fyj",
-        "max_hops": 3,
-        "max_spread": "0.5",
-        "per_hop_fee": "100000",
-        "router_addr": network["routerAddress"],
-        "whitelisted_tokens": [
-            TokenAsset(network["tokenAddresses"]["AAA"]).get_info().to_dict(),
-            TokenAsset(network["tokenAddresses"]["BBB"]).get_info().to_dict(),
-            TokenAsset(network["tokenAddresses"]["CCC"]).get_info().to_dict(),
-            TokenAsset(network["tokenAddresses"]["DDD"]).get_info().to_dict(),
-            NativeAsset("uluna").get_info().to_dict(),
-        ],
-        "whitelisted_fee_assets": [
-            TokenAsset(network["tokenAddresses"]["CCC"], "100000").get_asset(),
-            TokenAsset(network["tokenAddresses"]["AAA"], "200000").get_asset(),
-            NativeAsset("uluna", "300000").get_asset(),
-        ]
-    }
+    def instantiate_dca_contract(self, dca_code_id: int):
+        instantiate_msg = {
+            "factory_addr":
+            "terra1qnmggqv3u03axw69cn578hukqhl4f2ze2k403ykcdlhj98978u7stv7fyj",
+            "max_hops": 3,
+            "max_spread": "0.5",
+            "per_hop_fee": "100000",
+            "router_addr": self.network["routerAddress"],
+            "whitelisted_tokens": [
+                TokenAsset(self.network["tokenAddresses"]
+                           ["AAA"]).get_info().to_dict(),
+                TokenAsset(self.network["tokenAddresses"]
+                           ["BBB"]).get_info().to_dict(),
+                TokenAsset(self.network["tokenAddresses"]
+                           ["CCC"]).get_info().to_dict(),
+                TokenAsset(self.network["tokenAddresses"]
+                           ["DDD"]).get_info().to_dict(),
+                NativeAsset("uluna").get_info().to_dict(),
+            ],
+            "whitelisted_fee_assets": [
+                TokenAsset(self.network["tokenAddresses"]
+                           ["CCC"], "1000").get_asset(),
+                TokenAsset(self.network["tokenAddresses"]
+                           ["AAA"], "2000").get_asset(),
+                NativeAsset("uluna", "3000").get_asset(),
+            ]
+        }
 
-    contract_address = DCA(terra, wallet).instantiate(
-        dca_code_id, instantiate_msg)
+        contract_address = DCA(self.terra, self.wallet).instantiate(
+            dca_code_id, instantiate_msg)
 
-    network["dcaAddress"] = contract_address
-    write_artifact(network, "localterra")
-    print("dca contract_address=", contract_address)
+        self.network["dcaAddress"] = contract_address
+        write_artifact(self.network, "localterra")
+        print("dca contract_address=", contract_address)
 
+    def deploy_dca_contract(self):
+        code_id = self.upload_dca_contract()
+        self.instantiate_dca_contract(int(code_id))
 
-def deploy_dca_contract():
-    terra = LocalTerra()
-    wallet = terra.wallets["test1"]
+    def get_block_time_second(self) -> int:
+        from dateutil.parser import parse
+        time_string = self.terra.tendermint.block_info()[
+            'block']['header']['time']
+        date_time = parse(time_string)
+        return round(date_time.timestamp())
 
-    code_id = upload_dca_contract(terra, wallet)
-    instantiate_dca_contract(terra, wallet, int(code_id))
+    def create_test_orders(self):
+        dca = DCA(self.terra, self.wallet, self.network["dcaAddress"])
 
+        dca.execute_create_oder(
+            TokenAsset(self.network["tokenAddresses"]["AAA"], "1000000"),
+            NativeAsset("uluna").get_info(),
+            10, 10000, None)
 
-def get_block_time_second(terra: LCDClient) -> int:
-    from dateutil.parser import parse
-    time_string = terra.tendermint.block_info()['block']['header']['time']
-    date_time = parse(time_string)
-    return round(date_time.timestamp())
+        dca.execute_create_oder(
+            TokenAsset(self.network["tokenAddresses"]["AAA"], "1000000"),
+            TokenAsset(self.network["tokenAddresses"]["CCC"]).get_info(),
+            10, 10000, self.get_block_time_second()+10)
 
+    def add_user_tip_balance(self):
+        dca = DCA(self.terra, self.wallet, self.network["dcaAddress"])
 
-def create_test_orders():
-    network = read_artifact("localterra")
-    terra = LocalTerra()
-    test_wallet_1 = terra.wallets["test1"]
-
-    dca = DCA(terra, test_wallet_1, network["dcaAddress"])
-
-    dca.execute_create_oder(
-        TokenAsset(network["tokenAddresses"]["AAA"], "1000000"),
-        NativeAsset("uluna").get_info(),
-        10, 1000000, None)
-
-    dca.execute_create_oder(
-        TokenAsset(network["tokenAddresses"]["AAA"], "1000000"),
-        TokenAsset(network["tokenAddresses"]["CCC"]).get_info(),
-        10, 1000000, get_block_time_second(terra)+10)
-
-
-def add_user_tip_balance():
-    network = read_artifact("localterra")
-    terra = LocalTerra()
-    test_wallet_1 = terra.wallets["test1"]
-
-    dca = DCA(terra, test_wallet_1, network["dcaAddress"])
-
-    asset1 = TokenAsset(
-        network["tokenAddresses"]["CCC"], "1000000")
-    asset2 = NativeAsset("uluna", "1000000")
-    dca.execute_add_bot_tip([asset1, asset2])
-    # dca.execute_add_bot_tip(asset1)
+        asset1 = TokenAsset(
+            self.network["tokenAddresses"]["CCC"], "1000000")
+        asset2 = NativeAsset("uluna", "1000000")
+        dca.execute_add_bot_tip([asset1, asset2])
 
 
 if __name__ == "__main__":
-    # deploy_dca_contract()
-    # create_test_orders()
-    add_user_tip_balance()
-    pass
+    logging.basicConfig(level=logging.DEBUG)
+
+    s = Setup()
+    # s.deploy_dca_contract()
+    # s.create_test_orders()
+    # s.add_user_tip_balance()
